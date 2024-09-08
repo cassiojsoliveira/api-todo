0407dfac54f3193b75db70b3303ea742517b7b00
cassiojs <cassiojs.89@gmail.com>
2024-09-08 11:26:15 -0300

    base projeto

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import path from "path";
+import router from "./routes";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, "public")));
+
+server.use("/", router);
+
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});
